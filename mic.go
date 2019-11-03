237cf46f8fbc35422ee560c3b74cbe5f112c34da
cuixg <cuixg@51baibao.com>
2019-11-03 15:50:19 +0800

    default config

--- mic.go
+++ mic.go
@@ -1,5 +1,17 @@
 package mic
 
+import (
+	"github.com/micro/go-micro/config"
+	"github.com/micro/go-micro/config/source/env"
+)
+
 // 针对go-micro 框架的工具库和进一步封装， 降低使用门槛和学习成本
 
 // 主入口见 DefaultService
+
+// DefaultConfig 最常用的config方式， 从环境变量中读取
+func DefaultConfig() (config.Config, error) {
+	conf := config.NewConfig()
+	err := conf.Load(env.NewSource())
+	return conf, err
+}
