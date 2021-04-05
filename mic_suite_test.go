dbdf0b74eef6b0e515ef9df1617e1965ce6026a3
cuixg <cuixg@51baibao.com>
2021-04-05 11:34:47 +0800

    feat: event bus，封装micro消息系统，提高易用性

--- mic_suite_test.go
+++ mic_suite_test.go
@@ -1,8 +1,10 @@
 package mic_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +13,13 @@ func TestMic(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Mic Suite")
 }
+
+var ctl *gomock.Controller
+var cleaner func()
+var ctx context.Context
+var _ = BeforeEach(func() {
+	ctl = gomock.NewController(GinkgoT())
+	cleaner = ctl.Finish
+	ctx = context.Background()
+})
+var _ = AfterEach(func() { cleaner() })
