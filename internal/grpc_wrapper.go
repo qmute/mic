3406633b700e5542134d18a386b0e70f02102542
zhangbo <xiaobo1128@gmail.com>
2024-03-14 15:40:57 +0800

    refactor: grpc 任何错误都打印

--- internal/grpc_wrapper.go
+++ internal/grpc_wrapper.go
@@ -6,19 +6,18 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"go-micro.dev/v4/server"
-
-	"gitlab.51baibao.com/server/mic/v4/ut"
 )
 
 // GrpcErrLogWrapper serverWrapper to print err stack
 func GrpcErrLogWrapper(h server.HandlerFunc) server.HandlerFunc {
 	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		// 不能在包将错误返回，使用方有可能直接使用detail字段给用户返回
 		err := h(ctx, req, rsp)
 		if err != nil {
-			me, ok := ut.ParseMicError(err)
-			if ok && me.Code != 500 {
-				return err
-			}
+			// me, ok := ut.ParseMicError(err)
+			// if ok && me.Code != 500 {
+			// 	return err
+			// }
 
 			logrus.WithError(err).Errorf("rpc panic err %s.%s \n %+v\n", req.Service(), req.Endpoint(), err)
 			fmt.Printf("err %+v\n", err)
