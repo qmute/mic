8fdf28f5022059bf8f293a85297f1e85e6528bfc
zhangbo <xiaobo1128@gmail.com>
2024-12-30 12:08:25 +0800

    refactor: 订阅出错，打印payload与body

--- internal/subscribe_wrapper.go
+++ internal/subscribe_wrapper.go
@@ -14,7 +14,7 @@ func SubscribeErrLogWrapper(next server.SubscriberFunc) server.SubscriberFunc {
 	return func(ctx context.Context, msg server.Message) error {
 		err := next(ctx, msg)
 		if err != nil {
-			logrus.WithField("stack", string(debug.Stack())).Errorf("subscribe panic err from %s \n %+v\n", msg.Topic(), err)
+			logrus.WithField("stack", string(debug.Stack())).Errorf("subscribe panic err from %s \n %+v\n playload: %#v \n raw body: %s", msg.Topic(), err, msg.Payload(), string(msg.Body()))
 			fmt.Printf("panic %+v\n%s\n", err, string(debug.Stack()))
 		}
 		return err
